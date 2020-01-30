6ab1379cea789607a8ed2d303dee99c15affc807
Kai Salmen <myself@kaisalmen.de>
2020-01-30 22:10:23 +0100

    webgl_loader_assetpipeline_obj_stage: Make all models load again. GUI not working again, yet

--- docker/createDevEnv.sh
+++ docker/createDevEnv.sh
@@ -7,19 +7,13 @@ DIR_BASE=$(realpath ${DIR_ME}/..)
 if [[ ! -d ${DIR_BASE}/build ]]; then
   mkdir ${DIR_BASE}/build
 fi
-if [[ ! -d ${DIR_BASE}/examples/jsm/controls ]]; then
-  mkdir -p ${DIR_BASE}/examples/jsm/controls
-fi
-if [[ ! -d ${DIR_BASE}/examples/jsm/libs ]]; then
-  mkdir -p ${DIR_BASE}/examples/jsm/libs
-fi
-if [[ ! -d ${DIR_BASE}/examples/jsm/loaders ]]; then
-  mkdir -p ${DIR_BASE}/examples/jsm/loaders
-fi
 
 cp -f ${DIR_BASE}/node_modules/three/build/* ${DIR_BASE}/build
 cp -f ${DIR_BASE}/node_modules/three/examples/jsm/controls/TrackballControls.js ${DIR_BASE}/examples/jsm/controls/TrackballControls.js
+cp -f ${DIR_BASE}/node_modules/three/examples/jsm/controls/TrackballControls.d.ts ${DIR_BASE}/examples/jsm/controls/TrackballControls.d.ts
 cp -f ${DIR_BASE}/node_modules/three/examples/jsm/loaders/MTLLoader.js ${DIR_BASE}/examples/jsm/loaders/MTLLoader.js
 cp -f ${DIR_BASE}/node_modules/three/examples/jsm/loaders/MTLLoader.d.ts ${DIR_BASE}/examples/jsm/loaders/MTLLoader.d.ts
+cp -f ${DIR_BASE}/node_modules/three/examples/jsm/helpers/VertexNormalsHelper.js ${DIR_BASE}/examples/jsm/helpers/VertexNormalsHelper.js
+cp -f ${DIR_BASE}/node_modules/three/examples/jsm/helpers/VertexNormalsHelper.d.ts ${DIR_BASE}/examples/jsm/helpers/VertexNormalsHelper.d.ts
 cp -f ${DIR_BASE}/node_modules/three/examples/jsm/libs/dat.gui.module.js ${DIR_BASE}/examples/jsm/libs/dat.gui.module.js
 cp -f ${DIR_BASE}/node_modules/three/examples/jsm/libs/gunzip.module.min.js ${DIR_BASE}/examples/jsm/libs/gunzip.module.min.js
