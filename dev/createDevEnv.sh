8b6efb923f1d105d7216f1649d589db360b542c8
Kai Salmen <myself@kaisalmen.de>
2021-01-22 18:23:22 +0100

    Fix devEnv script

--- dev/createDevEnv.sh
+++ dev/createDevEnv.sh
@@ -41,8 +41,8 @@ cp -f ${DIR_BASE}/node_modules/three/examples/jsm/controls/TrackballControls.d.t
 cp -f ${DIR_BASE}/node_modules/three/examples/jsm/loaders/MTLLoader.js ${DIR_BASE}/examples/jsm/loaders/MTLLoader.js
 cp -f ${DIR_BASE}/node_modules/three/examples/jsm/loaders/MTLLoader.d.ts ${DIR_BASE}/examples/jsm/loaders/MTLLoader.d.ts
 cp -f ${DIR_BASE}/node_modules/three/examples/js/loaders/MTLLoader.js ${DIR_BASE}/examples/js/loaders/MTLLoader.js
-#cp -f ${DIR_BASE}/node_modules/three/examples/jsm/loaders/OBJLoader.js ${DIR_BASE}/examples/jsm/loaders/OBJLoader.js
-#cp -f ${DIR_BASE}/node_modules/three/examples/jsm/loaders/OBJLoader.d.ts ${DIR_BASE}/examples/jsm/loaders/OBJLoader.d.ts
+cp -f ${DIR_BASE}/node_modules/three/examples/jsm/loaders/OBJLoader.js ${DIR_BASE}/examples/jsm/loaders/OBJLoader.js
+cp -f ${DIR_BASE}/node_modules/three/examples/jsm/loaders/OBJLoader.d.ts ${DIR_BASE}/examples/jsm/loaders/OBJLoader.d.ts
 cp -f ${DIR_BASE}/node_modules/three/examples/js/loaders/OBJLoader.js ${DIR_BASE}/examples/js/loaders/OBJLoader.js
 cp -f ${DIR_BASE}/node_modules/three/examples/jsm/helpers/VertexNormalsHelper.js ${DIR_BASE}/examples/jsm/helpers/VertexNormalsHelper.js
 cp -f ${DIR_BASE}/node_modules/three/examples/jsm/helpers/VertexNormalsHelper.d.ts ${DIR_BASE}/examples/jsm/helpers/VertexNormalsHelper.d.ts
