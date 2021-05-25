4868a77e5bc516953a43c931b8be3c90f1689d55
Kai Salmen <mysel@kaisalmen.de>
2021-05-25 22:50:57 +0200

    Align rollup config and repository layout with three-wtm

---
+++ dev/verify/snowpack.config.js
@@ -0,0 +1,33 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+// This is bundled with npm package to allow direct usage
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+	mount: {
+		"libs/wwobjloader2": "/libs/wwobjloader2",
+		"public": "/",
+		"libs/three": "/libs/three",
+		"src": "/dist"
+	},
+	plugins: [
+		/* ... */
+	],
+	packageOptions: {
+		source: "local"
+	},
+	devOptions: {
+		open: "none",
+		port: 8086
+	},
+	buildOptions: {
+		/* ... */
+	},
+	optimize: {
+		/* ... */
+	},
+	exclude: [
+		/* ... */
+	]
+};
--- rollup.config.js
+++ rollup.config.js
@@ -4,81 +4,100 @@ import resolve from '@rollup/plugin-node-resolve';
 import { terser } from "rollup-plugin-terser";
 import { name, dependencies, devDependencies } from './package.json';
 
-// transformation instructions
-const patternOBJLoader2 = new RegExp('../../dist/loaders/OBJLoader2.js', 'g');
-const patternOBJLoader2Parallel = new RegExp('../../dist/loaders/OBJLoader2Parallel.js', 'g');
-const patternMtlObjBridge = new RegExp('../../dist/loaders/utils/MtlObjBridge.js', 'g');
-const patternOBJ2LoaderWorker = new RegExp('../../dist/loaders/workerTaskManager/worker/tmOBJLoader2.js', 'g');
-const packageModule = `../npm/${name}.module.js`;
-
 const copyConfig = {
-  targets: [
-    { src: 'public/index.html', dest: 'build/public' },
+  targets: buildCopyConfig(false).concat(buildCopyConfig(true)),
+  hook: 'closeBundle'
+};
+
+function buildCopyConfig(min) {
+  const basedir = min ? 'build/verifymin' : 'build/verify';
+  const examplesDir = basedir + '/public/examples';
+  const moduleReplacer = min ? '../libs/wwobjloader2/wwobjloader2.module.min.js' : '../libs/wwobjloader2/wwobjloader2.module.js';
+  const moduleReplacerWorker = min ? '../../libs/wwobjloader2/wwobjloader2.module.min.js' : '../../libs/wwobjloader2/wwobjloader2.module.js';
+
+  // transformation instructions: Required to verify examples work with bundled lib
+  const patternOBJLoader2 = new RegExp('../../dist/loaders/OBJLoader2.js', 'g');
+  const patternOBJLoader2Parallel = new RegExp('../../dist/loaders/OBJLoader2Parallel.js', 'g');
+  const patternMtlObjBridge = new RegExp('../../dist/loaders/utils/MtlObjBridge.js', 'g');
+  const patternOBJ2LoaderWorker = new RegExp('../../dist/loaders/workerTaskManager/worker/tmOBJLoader2.js', 'g');
+  return [
+    {
+      src: 'public/index.html',
+      dest: basedir + '/public'
+    },
     {
       src: 'public/examples/webgl_loader_obj2.html',
-      dest: 'build/public/examples',
+      dest: examplesDir,
       transform: (contents, filename) => {
         let str = contents.toString();
-        str = str.replace(patternMtlObjBridge, packageModule);
-        return str.replace(patternOBJLoader2, packageModule);
+        str = str.replace(patternMtlObjBridge, moduleReplacer);
+        return str.replace(patternOBJLoader2, moduleReplacer);
       }
     },
     {
       src: 'public/examples/webgl_loader_obj2_options.html',
-      dest: 'build/public/examples',
+      dest: examplesDir,
       transform: (contents, filename) => {
         let str = contents.toString();
-        str = str.replace(patternMtlObjBridge, packageModule);
-        str = str.replace(patternOBJLoader2, packageModule);
-        return str.replace(patternOBJLoader2Parallel, packageModule);
+        str = str.replace(patternMtlObjBridge, moduleReplacer);
+        str = str.replace(patternOBJLoader2, moduleReplacer);
+        return str.replace(patternOBJLoader2Parallel, moduleReplacer);
       }
     },
     {
       src: 'public/examples/webgl_loader_obj2_workermodulesupport.html',
-      dest: 'build/public/examples',
+      dest: examplesDir,
       transform: (contents, filename) => {
         let str = contents.toString();
-        return str.replace(patternOBJLoader2Parallel, packageModule);
+        return str.replace(patternOBJLoader2Parallel, moduleReplacer);
       }
     },
     {
       src: 'public/examples/wtm_potentially_infinite.html',
-      dest: 'build/public/examples',
+      dest: examplesDir,
       transform: (contents, filename) => {
         let str = contents.toString();
-        return str.replace(patternOBJ2LoaderWorker, packageModule);
+        return str.replace(patternOBJ2LoaderWorker, moduleReplacer);
       }
     },
     {
-      src: 'dev/build/snowpack.config.js',
-      dest: 'build'
+      src: min ? 'dev/verify/min/snowpack.config.js' : 'dev/verify/snowpack.config.js',
+      dest: basedir
     },
     {
       src: 'public/examples/main.css',
-      dest: 'build/public/examples'
+      dest: examplesDir
     },
     {
-      src: 'public/models/obj/female02/*',
-      dest: 'build/public/examples/models/obj/female02'
+      src: 'public/examples/models/obj/main/*',
+      dest: examplesDir + '/models/obj/main'
     },
     {
-      src: 'public/models/obj/male02/*',
-      dest: 'build/public/examples/models/obj/male02'
+      src: 'src/loaders/workerTaskManager/worker/*',
+      dest: examplesDir + '/worker/',
+      transform: (contents, filename) => {
+//        let str = contents.toString();
+//        str = str.replace(patternDefaultRouting, moduleReplacerWorker);
+//        str = str.replace(patternMaterialUtils, moduleReplacerWorker);
+//        return str.replace(patternTransportUtils, moduleReplacerWorker);
+        return contents;
+      }
     },
     {
-      src: 'public/models/obj/ninja/*',
-      dest: 'build/public/examples/models/obj/ninja'
+      src: 'node_modules/three',
+      dest: basedir + '/libs'
     },
     {
-      src: 'public/models/obj/cerberus/*',
-      dest: 'build/public/examples/models/obj/cerberus'
-    },
-    {
-      src: 'public/models/obj/walt/*',
-      dest: 'build/public/examples/models/obj/walt'
+      src: min ? 'build/wwobjloader2.module.min.js' : 'build/wwobjloader2.module.js',
+      dest: basedir + '/libs/wwobjloader2'
     }
   ]
 };
+
+const terserConfig = {
+  keep_classnames: true,
+  module: true
+}
 
 export default [
   // everything in one package
@@ -94,23 +113,27 @@ export default [
         format: 'cjs',
         file: `build/${name}.common.min.js`,
         exports: 'auto',
-        plugins: [terser()]
+        plugins: [
+            terser(terserConfig)
+        ]
       },
       {
         format: 'es',
-        file: `build/npm/${name}.module.js`,
+        file: `build/${name}.module.js`,
       },
       {
         format: 'es',
-        file: `build/npm/${name}.module.min.js`,
-        plugins: [terser()]
+        file: `build/${name}.module.min.js`,
+        plugins: [
+            terser(terserConfig)
+        ]
       }
     ],
     external: [ ...Object.keys(dependencies), ...Object.keys(devDependencies) ],
     plugins: [
       resolve(),
       babel(),
-      copy(copyConfig),
+      copy(copyConfig)
     ]
   }
 ];
