7642e42e24321b29bcfca72a08d2d449431f3047
Kai Salmen <mysel@kaisalmen.de>
2022-09-15 21:02:48 +0200

    Fix all remaining examples

--- packages/examples/vite.config.ts
+++ packages/examples/vite.config.ts
@@ -6,30 +6,31 @@ import path from 'path';
 // https://vitejs.dev/guide/build.html#multi-page-app
 
 export default defineConfig({
-    plugins: [
-    ],
-    build: {
-        rollupOptions: {
-            external: [
-                'three',
-                'three/examples/jsm/controls/TrackballControls',
-                'three/examples/jsm/helpers/VertexNormalsHelper',
-                'three/examples/jsm/loaders/MTLLoader'
-            ],
-            input: {
-                index: path.resolve(__dirname, 'index.html'),
-                obj2_basic: path.resolve(__dirname, 'obj2_basic.html'),
-                obj2parallel_basic: path.resolve(__dirname, 'obj2parallel_basic.html'),
-                obj2_options: path.resolve(__dirname, 'obj2_options.html'),
-                assetpipeline: path.resolve(__dirname, 'assetpipeline.html'),
-                assetpipeline_obj_stage: path.resolve(__dirname, 'assetpipeline_obj_stage.html')
-            },
-            plugins: [
-            ]
-        }
-    },
-    server: {
-        port: 8085,
-        host: '0.0.0.0'
-    }
+	plugins: [
+	],
+	build: {
+		rollupOptions: {
+			external: [
+				'three',
+				'three/examples/jsm/controls/TrackballControls',
+				'three/examples/jsm/helpers/VertexNormalsHelper',
+				'three/examples/jsm/loaders/MTLLoader'
+			],
+			input: {
+				index: path.resolve(__dirname, 'index.html'),
+				obj2_basic: path.resolve(__dirname, 'obj2_basic.html'),
+				obj2parallel_basic: path.resolve(__dirname, 'obj2parallel_basic.html'),
+				obj2_options: path.resolve(__dirname, 'obj2_options.html'),
+				obj2_bugverify: path.resolve(__dirname, 'obj2_bugverify.html'),
+				assetpipeline: path.resolve(__dirname, 'assetpipeline.html'),
+				assetpipeline_obj_stage: path.resolve(__dirname, 'assetpipeline_obj_stage.html')
+			},
+			plugins: [
+			]
+		}
+	},
+	server: {
+		port: 8085,
+		host: '0.0.0.0'
+	}
 });
